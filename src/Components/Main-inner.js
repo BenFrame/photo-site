903e649495246d3f0e9ecc566ce00b7318314707
Ben Frame <brewerytattooer@gmail.com>
2024-03-21 17:50:51 -0400

    Logo on inner added

--- src/Components/Main-inner.js
+++ src/Components/Main-inner.js
@@ -1,11 +1,17 @@
-
+import ablogo from '../ablogo.181d0c94.png'
 import { Link } from 'react-router-dom';
 
 export default function Main() {
     return (
-        <>
-            <h1>I LOST MY SHIT</h1>
-            <Link to="/" >BACK TO MY SPINNY</Link>
-        </>
-    )
+        <div>
+
+            <header className="App">
+                <img src={ablogo} className="App-logo" alt="logo" />
+                <h1>I LOST MY SHIT</h1>
+            </header>
+               <footer>
+                <Link to="/" >BACK TO MY SPINNY</Link>
+               </footer> 
+        </div>
+        )
 }
