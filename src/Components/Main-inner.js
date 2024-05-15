6548d8c8ccf25b38531aa61f3bc3ef0f0c7352cb
Ben Frame <brewerytattooer@gmail.com>
2024-05-15 13:20:55 -0400

    css styles updated on p tags

--- src/Components/Main-inner.js
+++ src/Components/Main-inner.js
@@ -21,16 +21,16 @@ export default function Main() {
             </header>
             <body>
                 <h2> About us: </h2>
-                <p>We breed and sell carpet pythons!</p>
-                <p>We are local to Rochester NY, but we ship nationwide!</p><br></br>
+                <p1>We breed and sell carpet pythons!</p1>
+                <p1>We are local to Rochester NY, but we ship nationwide!</p1><br></br>
 
-                <p>After taking in our first carpet python, we quickly fell in love with the species. 
+                <p1>After taking in our first carpet python, we quickly fell in love with the species. 
                 We would go to local shops and reptile expos and noticed there would always be lots 
                 of ball pythons, boas, and geckos... but rarely would we find carpet pythons! 
                 On the occasion that we did, the person selling it would generally not be the person 
                 who had bred the animal or known the parents and would not have much information about the snake that was for sale.
                 With a desire to share this awesome species with the reptile enthusiasts around us 
-                (and with a continuously expanding collection of animals reaching breeding age) we eventually decided to start AnB Reptiles in 2017!</p>
+                (and with a continuously expanding collection of animals reaching breeding age) we eventually decided to start AnB Reptiles in 2017!</p1>
             </body>
             <footer className = "Footer">
                 {/* <img  src={ablogo} className="App-logo-inner" alt="logo"/> */}
