01758a48c68bbdba921bd7f9753489dc876a5bc0
Ben Frame <brewerytattooer@gmail.com>
2024-05-22 14:40:32 -0400

    img url updates for species cards

--- src/Components/species-Cards/darwins-card.js
+++ src/Components/species-Cards/darwins-card.js
@@ -1,9 +1,9 @@
-import darwin from '../photos/Betty-coil.jpg'
+// import darwin from '../photos/Betty-coil.jpg'
 
 export const DarwinCard = () =>(
 
 <div class="Species-card">
-  <img src={darwin} alt="Avatar" />
+  <img src='https://i.imgur.com/BVlw89d.jpg' alt="Avatar" />
   <div >
     <h2>Darwins</h2>
     <p>Morelia spilota variegatta</p>
