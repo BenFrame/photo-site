c3c085f12aea5f8de2c0e34f47e01853a9053f82
Ben Frame <brewerytattooer@gmail.com>
2024-05-23 15:19:25 -0400

    Species card updates completed

--- src/Components/pages/collection.js
+++ src/Components/pages/collection.js
@@ -2,15 +2,15 @@ import ablogo from '../photos/ablogo.181d0c94.png'
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../App.css';
-import { JungleCard } from '../species-Cards/jungles-card';
-import { PapuanCard } from '../species-Cards/papuan-ij-card';
-import { DarwinCard } from '../species-Cards/darwins-card';
-import SpeciesCard from '../species-Cards/species-card';
+import SpeciesCard from '../species-card';
 
 export const Collection = () =>{
     const bredli = 'https://i.imgur.com/50gNhXJ.jpg'
     const novas = 'https://i.imgur.com/PvIyMNH.jpg'
     const coastal = 'https://i.imgur.com/Pm4QcEN.jpg'
+    const papuan = 'https://i.imgur.com/d7SYaMD.jpg'
+    const jungle = 'https://i.imgur.com/sCRUKTU.jpg'
+    const darwin = 'https://i.imgur.com/BVlw89d.jpg'
     return (
         <div >
             <header className="App-header-inner">
@@ -30,21 +30,33 @@ export const Collection = () =>{
             </header>
                 <section className='Card-section'>
                     <SpeciesCard
-                        img={bredli}
-                        commonName='Bredli'
-                        scientificName='Morelia bredli' />
+                        img={ bredli }
+                        commonName= 'Bredli'
+                        scientificName= 'Morelia bredli' />
                     <SpeciesCard
-                        img={novas}
-                        commonName='Nova Guenea'
-                        scientificName='Morelia spilota variegata'/>
+                        img={ novas }
+                        commonName= 'Nova Guenea'
+                        scientificName= 'Morelia spilota variegata'/>
                     <SpeciesCard 
-                        img={coastal}
-                        commonName='Coastals'
-                        scientificName='Morelia spilota mcdowelli'
+                        img={ coastal }
+                        commonName= 'Coastals'
+                        scientificName= 'Morelia spilota mcdowelli'
                     />
-                    <PapuanCard/>
-                    <JungleCard/>
-                    <DarwinCard/>
+                    <SpeciesCard
+                        img={ papuan }
+                        commonName= 'Papuan(Irian Jaya)'
+                        scientificName= 'Morelia spilota variegata'
+                    />
+                    <SpeciesCard
+                        img={ jungle }
+                        commonName= 'Jungles'
+                        scientificName= 'Morelia spilota cheynei'
+                    />
+                    <SpeciesCard
+                        img={ darwin }
+                        commonName= 'Darwins'
+                        scientificName= 'Morelia spilota variegata'
+                    />
                 </section>
             <footer className = "Footer">
                 <div>
