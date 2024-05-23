77877f4ba0411b9fbaba9e403ca2fa6e1abf3686
Ben Frame <brewerytattooer@gmail.com>
2024-05-23 14:49:01 -0400

    Stand alone bredli card removed

--- src/Components/pages/collection.js
+++ src/Components/pages/collection.js
@@ -1,16 +1,17 @@
 import ablogo from '../photos/ablogo.181d0c94.png'
+import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../App.css';
 import { CoastalCard } from '../species-Cards/coastals-card';
 import { JungleCard } from '../species-Cards/jungles-card';
-import { BredliCard } from '../species-Cards/bredli-card';
 import { PapuanCard } from '../species-Cards/papuan-ij-card';
 import { NovaGuineaCard } from '../species-Cards/nova-card';
 import { DarwinCard } from '../species-Cards/darwins-card';
 import SpeciesCard from '../species-Cards/species-card';
-import bredli from 'https://i.imgur.com/50gNhXJ.jpg'
+// import bredli from 'https://imgur.com/50gNhXJ'
 
 export const Collection = () =>{
+    const bredli = 'https://i.imgur.com/50gNhXJ.jpg'
     return (
         <div >
             <header className="App-header-inner">
@@ -30,7 +31,9 @@ export const Collection = () =>{
             </header>
                 <section className='Card-section'>
                     <SpeciesCard
-                        img={bredli} />
+                        img={bredli}
+                        commonName='Bredli'
+                        scientificName='Morelia bredli' />
                     <NovaGuineaCard/>
                     <CoastalCard/>
                     <PapuanCard/>
--- src/Components/species-Cards/species-card.js
+++ src/Components/species-Cards/species-card.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function SpeciesCard(props) {
     return <div class="Species-card">
-    <img alt="snakes" >{ props.img } </img>
+    <img src={ props.img } alt='img'/>
     <div >
       <h2> { props.commonName } </h2>
       <p> { props.scientificName } </p>
@@ -10,4 +10,4 @@ function SpeciesCard(props) {
   </div>
 }
 
-export default SpeciesCard()
+export default SpeciesCard
