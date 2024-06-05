58a4a7525ef35065b5c79377bdcad316972db00c
Ben Frame <brewerytattooer@gmail.com>
2024-06-05 14:26:21 -0400

    Coastal image data added

--- src/Components/photos/coastal-image-data.js
+++ src/Components/photos/coastal-image-data.js
@@ -1,9 +1,39 @@
 const coastalImageData = [
     {
         url: 'https://i.imgur.com/Pm4QcEN.jpg',
-        title: '',
+        title: 'Brisbane',
         description: '2020 Male',
-        producedBy:'Produced by: Billy Hunt'
+        producedBy:'Produced by: Brandon Wheeler'
+    },
+    {
+        url: 'https://i.imgur.com/Wlr2DD0.jpg',
+        title: 'Caramel Tiger',
+        description: '2019 Female',
+        producedBy:'Produced by: Jason Baylin'
+    },
+    {
+        url: 'https://i.imgur.com/khNXdrD.jpg',
+        title: 'Caramel Tiger',
+        description: '2018 Male',
+        producedBy:'Produced by: Todd Dyer'
+    },
+    {
+        url: 'https://i.imgur.com/kHKXkYh.jpg',
+        title: 'Axanthic',
+        description: '2020 Female',
+        producedBy:'Produced by: Riley Jimison'
+    },
+    {
+        url: 'https://i.imgur.com/MffX8UV.jpg',
+        title: 'Axanthic',
+        description: '2020 Male',
+        producedBy:'Produced by: Riley Jimison'
+    },
+    {
+        url: 'https://i.imgur.com/u7RxbEF.jpg',
+        title: 'MBB-Red',
+        description: '2020 Female',
+        producedBy:'Produced by: Martin Rosemond'
     },
    
 ]
