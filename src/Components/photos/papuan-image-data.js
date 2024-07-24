48e7456264b58d3a6a87664a28f08ef26dad9df9
Ben Frame <brewerytattooer@gmail.com>
2024-07-24 16:09:43 -0400

    Papuan image data updated

--- src/Components/photos/papuan-image-data.js
+++ src/Components/photos/papuan-image-data.js
@@ -6,6 +6,12 @@ const papuanImageData = [
         producedBy:'Produced by: Ania Sitarski'
     },
     {
+        url: 'https://i.imgur.com/xyPmeKp.jpg',
+        title: 'Undocumented',
+        description: '2018 Female',
+        producedBy:'Produced by: AnB Reptiles'
+    },
+    {
         url: 'https://i.imgur.com/d7SYaMD.jpg',
         title: 'Undocumented',
         description: '2018 Male',
@@ -15,6 +21,12 @@ const papuanImageData = [
         url: 'https://i.imgur.com/wMYoY80.jpg',
         title: 'PC/Mpenn/Hann/VPI',
         description: '2022 Female',
+        producedBy:'Produced by: Jacob Bratz'
+    },
+    {
+        url: 'https://i.imgur.com/HZut5Oh.jpg',
+        title: 'PC/Mpenn/Hann/VPI',
+        description: '2022 Male',
         producedBy:'Produced by: Jacob Bratz'
     },
    
